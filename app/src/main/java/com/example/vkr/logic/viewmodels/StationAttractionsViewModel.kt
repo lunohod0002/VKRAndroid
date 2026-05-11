ee139631adaf55447257d027f4d50bf838c5657c
George Zhironkin <lunohod0002@gmail.com>
2026-05-11 10:47:02 +0300

    edit station_attractions.xml

--- app/src/main/java/com/example/vkr/logic/viewmodels/StationAttractionsViewModel.kt
+++ app/src/main/java/com/example/vkr/logic/viewmodels/StationAttractionsViewModel.kt
@@ -24,18 +24,18 @@ class StationAttractionsViewModel(
 
     fun getStationAttractions(stationId:Long) {
         viewModelScope.launch(Dispatchers.IO) {
-//            val attractions = listOf(
-//                StationAttractionInfo(1, "Зоопарк", 390, "https://s0.rbk.ru/v6_top_pics/media/img/1/14/756594550679141.webp",1500),
-//                StationAttractionInfo(2, "Музей космонавтики", 850, "https://cdn.iz.ru/sites/default/files/news-2018-12/2880px-Colosseum_in_Rome%2C_Italy_-_April_2007.jpg",800),
-//                StationAttractionInfo(3, "Парк Горького", 210, "https://safety-rest.ru/upload/iblock/655/655861e57c7196758fe81b8c0f19a436.jpg",0),
-//                StationAttractionInfo(4, "Третьяковская галерея", 2100,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwyIS3lgTauIU1J_3ECsDxnqF8jyaIlcBQTg&s", 1200),
-//                StationAttractionInfo(5, "Бащня", 2100,"https://depositphotos-blog.s3.eu-west-1.amazonaws.com/uploads/2017/07/Depositphotos_5593372_m-2015.jpg", 500)
-//
-//            )
-//            resultLiveMutable.postValue(attractions)
+            val attractions = listOf(
+                StationAttractionInfo(1, "Зоопарк", 390, "https://s0.rbk.ru/v6_top_pics/media/img/1/14/756594550679141.webp",1500),
+                StationAttractionInfo(2, "Музей космонавтики", 850, "https://cdn.iz.ru/sites/default/files/news-2018-12/2880px-Colosseum_in_Rome%2C_Italy_-_April_2007.jpg",800),
+                StationAttractionInfo(3, "Парк Горького", 210, "https://safety-rest.ru/upload/iblock/655/655861e57c7196758fe81b8c0f19a436.jpg",0),
+                StationAttractionInfo(4, "Третьяковская галерея", 2100,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwyIS3lgTauIU1J_3ECsDxnqF8jyaIlcBQTg&s", 1200),
+                StationAttractionInfo(5, "Бащня", 2100,"https://depositphotos-blog.s3.eu-west-1.amazonaws.com/uploads/2017/07/Depositphotos_5593372_m-2015.jpg", 500)
 
-            val attractions = stationApi.getStationAttractions(stationId)
-            resultLiveMutable.postValue(attractions.body()?.content)
+            )
+            resultLiveMutable.postValue(attractions)
+
+//            val attractions = stationApi.getStationAttractions(stationId)
+//            resultLiveMutable.postValue(attractions.body()?.content)
 
             }
         }
